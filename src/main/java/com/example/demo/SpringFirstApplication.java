2a9b2ae6494a63650492d86d2714f99c779e5ff6
Yellareddy07 <131944447+Yellareddy07@users.noreply.github.com>
2026-03-04 18:37:00 +0530

    Update SpringFirstApplication.java

--- src/main/java/com/example/demo/SpringFirstApplication.java
+++ src/main/java/com/example/demo/SpringFirstApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;  // ← ADD THIS
+
 
 @SpringBootApplication
 public class SpringFirstApplication extends SpringBootServletInitializer{
