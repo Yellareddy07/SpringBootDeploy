49a4278a63aa26b708964a28becd7e73b32e7b69
Yellareddy07 <131944447+Yellareddy07@users.noreply.github.com>
2026-03-04 16:16:02 +0530

    Update SpringFirstApplication.java

--- src/main/java/com/example/demo/SpringFirstApplication.java
+++ src/main/java/com/example/demo/SpringFirstApplication.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-/*
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,18 +12,4 @@ public class SpringFirstApplication {
 
 }
 
-#this is for external tomcat server
-*/
 
-/* this is for the tomcat container present in jenkins */
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class SpringFirstApplication extends SpringBootServletInitializer {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringFirstApplication.class, args);
-    }
-}
